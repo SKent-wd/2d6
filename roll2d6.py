d9023cf8b138b5e2f5e485156d43f0cefacfa1b0
s-kent <spkent@hotmail.com>
2025-06-13 14:36:42 +0100

    Prevented potential issues with roll totals and messages. Added option to generate bar charts based on result values.

--- roll2d6.py
+++ roll2d6.py
@@ -1,38 +1,44 @@
-#import random
 import datetime
+import numpy as np
 from numpy import random
+import sys
+import matplotlib
+import matplotlib.pyplot as plt
 
-#Global variables
-makeRoll = True  #Flag for allowing the dice roll
-rollCount = 0  #Count the number of dice rolls
-rollMessage = "Now make your move."  #Default message
+matplotlib.use('Agg')
+
+# Global variables
+makeRoll = True  # Flag for allowing the dice roll
+rollCount = 0  # Count the number of dice rolls
+rollMessage = "Now make your move."  # Default message
 
 
-#Function simulates rolling 2 6-sided dice, displaying the result and a message
+# Function simulates rolling 2 6-sided dice, displaying the result and a message
 def roll_the_dice():
     global makeRoll
     global rollCount
     global rollMessage
 
     if rollCount < 3:
-        #Below code can be used with NumPy - to be implemented later
-        roll = random.randint(1, 7, size=(2))
-        d6_1 = int(roll[0])  #Dice 1
-        d6_2 = int(roll[1])  #Dice 2
+        # Below code can be used with NumPy - to be implemented later
+        roll = random.randint(1, 7, size=2)
+        d6_1 = int(roll[0])  # Dice 1
+        d6_2 = int(roll[1])  # Dice 2
 
-        rollCount += 1  #Increment the dice roll count
+        rollCount += 1  # Increment the dice roll count
 
-        rolled_total = d6_1 + d6_2  #Add the dice roll results together
+        rolled_total = d6_1 + d6_2  # Add the dice roll results together
 
-        rollMessage = decide_message(rolled_total)  #Set message
+        rollMessage = decide_message(rolled_total)  # Set message
 
         print("Rolling 2 6-sided dice gives... ")
         print(str(d6_1) + " and " + str(d6_2) + ", making " + str(rolled_total) + ".")
         print(rollMessage)
 
-        record_roll(d6_1, d6_2)  #Log the rolled numbers
+        record_roll(d6_1, d6_2)  # Log the rolled numbers
+        plot_roll(d6_1, d6_2)  # Plot the rolled numbers
 
-        #Stop rolling if the result is not 2 sixes
+        # Stop rolling if the result is not 2 sixes
         if rolled_total != 12:
             makeRoll = False
     else:
@@ -40,9 +46,19 @@ def roll_the_dice():
         print("...or not. I think you've had enough turns...!")
 
 
-#Function sets the output message based on the numerical value given
+# Function sets the output message based on the numerical value given
 def decide_message(rolled_total=0):
     global rollMessage
+    global rollCount
+
+    # Catch and stop the process if results are out of range
+    if rolled_total < 2 or rolled_total > 12:
+        sys.exit("Total returned as " + str(rolled_total) + " - totals can only be from 2 to 12.")
+
+    # If this is a bonus roll, update the default message
+    if rollCount > 1:
+        rollMessage = "Now make your next move."  # Default bonus roll message
+
     if rolled_total == 2:
         rollMessage = "Snake eyes!"
     elif rolled_total == 3:
@@ -62,9 +78,9 @@ def decide_message(rolled_total=0):
     return rollMessage
 
 
-#Writes the results into a text file
+# Writes the results into a text file
 def record_roll(d6_1=0, d6_2=0):
-    rolled_total = d6_1 + d6_2  #Added the dice rolls together
+    rolled_total = d6_1 + d6_2  # Added the dice rolls together
     now_val = datetime.datetime.now()
     now_string = now_val.strftime("%Y%m%d")
     now_txt = now_val.strftime("%Y-%m-%d %H:%M:%S")
@@ -73,14 +89,35 @@ def record_roll(d6_1=0, d6_2=0):
     f.close()
 
 
-#Prompts user to start the dice roll
+# Creates a plot and saves image based on
+def plot_roll(d6_1=0, d6_2=0):
+    make_plot = input("Create plot based on roll (y/n)?")  # Ask user to confirm plot creation
+    if make_plot == "Y" or make_plot == "y":
+        rolled_total = d6_1 + d6_2  # Added the dice rolls together
+        now_val = datetime.datetime.now()
+        now_string = now_val.strftime("%Y%m%d%H%M%S")
+
+        x = np.array(["Die 1", "Die 2", "Total"])
+        y = np.array([d6_1, d6_2, rolled_total])
+
+        plt.bar(x, y)
+        plt.savefig("roll_plots/roll_plot_" + now_string + ".png")
+
+        print("roll_plot_" + now_string + ".png has been created.")
+    elif make_plot == "N" or make_plot == "n":
+        pass
+    else:
+        print("Please type either Y or N.")  # Remind user of valid options
+
+
+# Prompts user to start the dice roll
 while makeRoll:
-    startRoll = input("Roll the dice (y/n)?")  #Ask user to confirm dice roll
+    startRoll = input("Roll the dice (y/n)?")  # Ask user to confirm dice roll
 
     if startRoll == "Y" or startRoll == "y":
-        roll_the_dice()  #User has agreed to roll the dice
+        roll_the_dice()  # User has agreed to roll the dice
     elif startRoll == "N" or startRoll == "n":
-        print("Oh, okay then.")  #No dice roll, stop process
+        print("Oh, okay then.")  # No dice roll, stop process
         break
     else:
-        print("Um, that's not a Y or an N?")  #Remind user of valid options
+        print("Um, that's not a Y or an N?")  # Remind user of valid options
